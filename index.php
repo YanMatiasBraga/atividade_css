127f0809a22ecbd08f35dbcc0b7e87d1164e9c2a
Yan Matias Braga <181710027@senacinformatica.br>
2019-03-21 10:57:02 -0300

    atividade_site_css

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="estilo.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+     
+        <div id="div01"> </div>
+        <div id="div02"> </div>
+        <div id="logo"><a href="#"><img src="imagens/Logo_apple.png" alt=""/></a></div>
+        <div id="store"><a href="#"><img src="imagens/search.png" alt=""/></a></div>
+        <div id="search"><a href="#"><img src="imagens/store.png" alt=""/></a></div>
+        <ul id="menuTopo">
+           <li><a href="#">Mac</a></li>
+           <li><a href="#">iPad</a></li>
+           <li><a href="#">iPhone</a></li>
+           <li><a href="#">Watch</a></li>
+           <li><a href="#">TV</a></li>
+          <li><a href="#">Music</a></li>
+          <li><a href="#">Support</a></li>
+        </ul>
+        
+        <div id="banner">
+        <div id="letras">
+            <h2>iPad Air Power.</h2>
+            <h2> isn’t just for the pros</h2>
+        </div>
+            </div>
+        
+        <div>
+        <img class="iphonex" src="imagens/iphone_x.png"/>
+        </div>
+        <div id="iphone">
+            <h2>IphoneX</h2>
+        </div>
+    </body>
+</html>
